90f31deb7df07791e4fba0fe77ec9e0357cb4660
Gonzalo G. <gonzalogarcia035@gmail.com>
2025-04-17 11:30:05 -0300

    se crea prompt y mensaje para ver el numero mayor

---
+++ js/06-condicional.js
@@ -0,0 +1,7 @@
+const numero1 = parseInt(prompt("Ingrese un numero"));
+numero2 = parseInt(prompt("Ingrese un segundo numero"));
+if(numero1 > numero2){
+    document.writeln("El numero mayor es: "+ numero1);
+} else {
+document.writeln("El numero mayor es: "+ numero2);
+}
