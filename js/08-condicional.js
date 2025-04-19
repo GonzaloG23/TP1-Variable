a2589e4f8128bd59f402d035ad337aa2d88b1244
Gonzalo G. <gonzalogarcia035@gmail.com>
2025-04-19 16:05:36 -0300

    se crea condicional con %

---
+++ js/08-condicional.js
@@ -0,0 +1,4 @@
+const numero1 = parseInt(prompt("Ingrese un numero"));
+if(numero1%2==0){
+    document.writeln("El numero es divisible por 2");
+} else{ document.writeln("El numero no es divisible por 2");}
